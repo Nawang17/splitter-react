a10763fffb3347d6e717c065b42c04e6d0f5e3e6
Nawang17 <nawangofficial1010@gmail.com>
2021-07-30 14:52:42 -0400

    wfaw

--- src/App.js
+++ src/App.js
@@ -1,51 +1,44 @@
-import Main from './Main'
-import React, {useState} from 'react';
-import './App.css';
+import Main from "./Main";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [bill ,setbill] = useState('0')
-  const [people,setpeople] = useState(1)
-  const [tip,settip] = useState('0')
-  const [total,settotal] = useState('0')
-  const [tipper,settipper] = useState('0')
-let totalnum = ''
-let tipamount = ''
-  if (parseInt(bill) && parseInt(people) > 0){
-    totalnum = (parseInt(bill) / parseInt(people))
+  const [bill, setbill] = useState("0");
+  const [people, setpeople] = useState(1);
+  const [tip, settip] = useState("0");
+  const [total, settotal] = useState("0");
+  const [tipper, settipper] = useState("0");
+  let totalnum = "";
+  let tipamount = "";
+  if (parseInt(bill) && parseInt(people) > 0) {
+    totalnum = parseInt(bill) / parseInt(people);
   }
 
-   totalnum = totalnum || 0
+  totalnum = totalnum || 0;
 
-let tipPercent = ''  
+  let tipPercent = "";
 
-tipPercent = (parseInt(tipper)/100) * totalnum
+  tipPercent = (parseInt(tipper) / 100) * totalnum;
 
-tipamount = totalnum + tipPercent
-
-
-
-
+  tipamount = totalnum + tipPercent;
 
   return (
-    <div>    
-    <header className='heading'> SPLITTER</header>  
-    <div className='main' >
-    
-     <Main setbill={setbill}
-     setpeople={setpeople}
-     totalnum = {totalnum}
-     total={total}
-     tipamount ={tipamount}
-     tipPercent = {tipPercent}
-     settipper ={settipper}
-     tip={tip}
-     />
-    
+    <div>
+      <header className="heading"> SPLITTER</header>
+      <div className="main">
+        <Main
+          setbill={setbill}
+          setpeople={setpeople}
+          totalnum={totalnum}
+          total={total}
+          tipamount={tipamount}
+          tipPercent={tipPercent}
+          settipper={settipper}
+          tip={tip}
+        />
+      </div>
+      <footer>Copyright © 2021, Nawang Sherpa. All Rights Reserved</footer>
     </div>
-    <footer>Copyright © 2021, Nawang Sherpa. All Rights Reserved</footer>
-    </div>
-
-
   );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,82 +1,120 @@
-import React from 'react'
-import './App.css'
-const Main = ({setbill, settipper,setpeople,total, tip,totalnum, tipamount ,tipPercent}) => {
-
-    return (
-     
-           <div className="main-container">
-            <div className="left-container">
-               <h3 className='text-first bill'>Bill</h3>
-               <form className='first-form' >
-                  <input onChange ={(event)=>{
-     setbill(event.target.value)}} className='first-input first'type='number' placeholder="$"/>
-  
-               </form>
-               <h3 className='text-first'>Select Tip %</h3>
-               <div className="buttons">
-                  <button  onClick={()=>{
-                      settipper(15)
-                  
-                  }}   className='percent-button'>15%</button>
-                  <button onClick={(event)=>{
-                      settipper(18)
-                      
-                     
-                 
-                  }}  className='percent-button'>18%</button>
-                  <button  onClick={()=>{
-                     
-                      settipper(20)
-            
-                  }} className='percent-button'>20%</button>
-                  <button onClick={()=>{
-                      settipper(25)
-                    
-                  }} className='percent-button'>25%</button>
-                  <button onClick={(even)=>{
-                      settipper(30)
-                     
-                  }} className='percent-button'>30%</button>
-                  <input  onChange ={(event)=>{
-     settipper(event.target.value)}} className='custom-button percent-button'type="number" placeholder="Custom"/>
-               </div>
-               <h3 className='text-first'>Number of People</h3>
-               <input  onChange ={(event)=>{
-     setpeople(event.target.value)
-     }} className=' first people'type="number"/>
+import React from "react";
+import "./App.css";
+const Main = ({
+  setbill,
+  settipper,
+  setpeople,
+  total,
+  tip,
+  totalnum,
+  tipamount,
+  tipPercent,
+}) => {
+  return (
+    <div className="main-container">
+      <div className="left-container">
+        <h3 className="text-first bill">Bill</h3>
+        <form className="first-form">
+          <input
+            onChange={(event) => {
+              setbill(event.target.value);
+            }}
+            className="first-input first"
+            type="number"
+            placeholder="$"
+          />
+        </form>
+        <h3 className="text-first">Select Tip %</h3>
+        <div className="buttons">
+          <button
+            onClick={() => {
+              settipper(15);
+            }}
+            className="percent-button"
+          >
+            15%
+          </button>
+          <button
+            onClick={(event) => {
+              settipper(18);
+            }}
+            className="percent-button"
+          >
+            18%
+          </button>
+          <button
+            onClick={() => {
+              settipper(20);
+            }}
+            className="percent-button"
+          >
+            20%
+          </button>
+          <button
+            onClick={() => {
+              settipper(25);
+            }}
+            className="percent-button"
+          >
+            25%
+          </button>
+          <button
+            onClick={(even) => {
+              settipper(30);
+            }}
+            className="percent-button"
+          >
+            30%
+          </button>
+          <input
+            onChange={(event) => {
+              settipper(event.target.value);
+            }}
+            className="custom-button percent-button"
+            type="number"
+            placeholder="Custom"
+          />
+        </div>
+        <h3 className="text-first">Number of People</h3>
+        <input
+          onChange={(event) => {
+            setpeople(event.target.value);
+          }}
+          className=" first people"
+          type="number"
+        />
+      </div>
+      <div className="right-container">
+        <div className="rightmain">
+          <div className="tipamount">
+            <div className="tip1">
+              <p>Tip Amount</p>
+              <p className="person1">/ person</p>
             </div>
-            <div className="right-container">
-               <div className="rightmain">
-                  <div className="tipamount">
-                     <div className="tip1">
-                        <p >Tip Amount</p>
-                        <p  className ='person1'>/ person</p>
-                     </div>
-                     <div className="tip2">
-                        <p >Total</p>
-                        <p  className ='person2' >/ person</p>
-                     </div>
-                  </div>
-                  <div className="dollar">
-                     <h2 className='dol-1'>${tipPercent.toFixed(2)}</h2>
-                     <h2 className='dol-2'>${tipamount.toFixed(2)}</h2>
-                  </div>
-               </div>
-           
-               <button  onClick={()=>{
-                      settipper(0)
-                      setbill(0)
-                      setpeople(0)
-                 
-            
-                    
-
-
-                  }} className='reset-btn'>RESET</button>
+            <div className="tip2">
+              <p>Total</p>
+              <p className="person2">/ person</p>
             </div>
-         </div>
-     
-    )
-}
+          </div>
+          <div className="dollar">
+            <h2 className="dol-1">${tipPercent.toFixed(2)}</h2>
+            <h2 className="dol-2">${tipamount.toFixed(2)}</h2>
+          </div>
+        </div>
 
-export default Main
+        <button
+          onClick={() => {
+            settipper(0);
+            setbill(0);
+            setpeople(0);
+          }}
+          className="reset-btn"
+        >
+          RESET
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
